dfcb20f6ca27f3cb091c313b41136b920a05b836
Anttoha <167896136+Antoha2012pro@users.noreply.github.com>
2025-02-27 17:54:39 +0100

    fix-3

--- src/js/modal-header.js
+++ src/js/modal-header.js
@@ -1,15 +1,15 @@
-(() => {
-  const refs = {
-    openModalBtn: document.querySelector("[data-modal-header-open]"),
-    closeModalBtn: document.querySelector("[data-modal-header-close]"),
-    modal: document.querySelector("[data-modal-header]"),
-  };
+// (() => {
+//   const refs = {
+//     openModalBtn: document.querySelector("[data-modal-header-open]"),
+//     closeModalBtn: document.querySelector("[data-modal-header-close]"),
+//     modal: document.querySelector("[data-modal-header]"),
+//   };
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
+//   refs.openModalBtn.addEventListener("click", toggleModal);
+//   refs.closeModalBtn.addEventListener("click", toggleModal);
 
-  function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
-    document.body.classList.toggle("no-scroll");
-  }
-})();
+//   function toggleModal() {
+//     refs.modal.classList.toggle("is-hidden");
+//     document.body.classList.toggle("no-scroll");
+//   }
+// })();
